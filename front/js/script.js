0ab26b53c6907379c30f4eb55b64037e89f3d186
Pollux911 <antoine.gourdon91@gmail.com>
2022-01-27 17:08:28 +0100

    added script.js

--- front/js/script.js
+++ front/js/script.js
@@ -4,6 +4,41 @@ fetch("http://localhost:3000/api/products")
             return res.json();
         }
     })
-    .then(function(value){
-        console.log(value);
+    .then(function(apiData){
+        for(let product of apiData) {
+            let apiProduct = displayProduct(product);
+            const items = document.getElementById('items');
+            items.appendChild(apiProduct);
+        }
+
     })
+    .catch(function(err) {
+        console.log("error with the API")
+    });
+
+function displayProduct(product) {
+    const link = document.createElement('a');
+    link.setAttribute('href', './product.html?id=' + product._id);
+
+    const article = document.createElement('article');
+
+    const image = document.createElement('img');
+    image.setAttribute('alt', product.altTxt + '');
+    image.setAttribute('src', product.imageUrl)
+    article.appendChild(image);
+
+    const productName = document.createElement('h3');
+    productName.classList.add(productName)
+    productName.innerText = product.name;
+    article.appendChild(productName)
+
+    const productDescription = document.createElement('p');
+    productDescription.classList.add(productDescription);
+    productDescription.innerText = product.description;
+    article.appendChild(productDescription);
+
+    link.appendChild(article);
+
+    return link
+}
+
